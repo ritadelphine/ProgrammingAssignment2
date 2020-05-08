d660b3ebf8fe89cc102f89fd1d9c6a367abb8259
ritadelphine <ritadelphinemaiko@gmail.com>
2020-05-08 14:02:49 +0200

    Rita: functions makeCacheMatrix, CacheSolve created and script testing the functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+#For this assignment two functions are used to inverse a matrix, cache the results, and to retrieve it. 
 
-## Write a short comment describing this function
+#makeCacheMatrix creates an inverse matrix and cache's this inv matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  imatrix <- NULL
+  set <- function(y){
+    x <<- y
+    imatrix <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) imatrix <<-inverse
+  getinv <- function() imatrix
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function retrieves the cached inverse matrix, checks if the object is not a null object, and prints the result.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  imatrix <- x$getinv()
+  if(!is.null(imatrix)) {
+    message("getting cached data")
+    return(imatrix)
+  }
+  data <- x$get()
+  imatrix <- solve(data, ...)
+  x$setinv(imatrix)
+  imatrix
 }
+
+# Check for if the functions work
+mymatrix <- matrix(rnorm(9), 3 , 3)
+myinvmatrix <- makeCacheMatrix(mymatrix)
+cacheSolve(myinvmatrix)
